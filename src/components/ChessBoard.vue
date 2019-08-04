6e96ba4bb18ac5cabd2c0bb646e03d8595fa64c9
Kanwalnoor Singh <kanwalnoor.s98@gmail.com>
2019-08-05 08:26:48 +1000

    img test

---
+++ src/components/ChessBoard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="board">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChessBoard",
+  props: {
+    msg: Image
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="home">
     <HelloWorld msg="Hello World" />
+    <img src="../assets/chessboard.jpg"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+
 
 export default {
   name: "home",
