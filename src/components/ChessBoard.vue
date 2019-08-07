49e4226629a5a88f4de7ab129db76d1bc4049e3e
Kanwalnoor Singh <kanwalnoor.s98@gmail.com>
2019-08-07 12:04:23 +1000

    diving board into areas; testing placement of pieces

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,13 +1,39 @@
 <template>
-    <div>
-        <img
+    <div class="board-area">
+        <img class="board"
             src="../assets/chessboard.jpg"
         />
+    <Piece class="piece"/>
     </div>
 </template>
 
 <script>
+
+import Piece from "../components/Piece.vue"
+
 export default {
-    name: 'ChessBoard'
+    name: 'ChessBoard',
+    components: {Piece
+    }
 };
 </script>
+
+<style scoped>
+.board-area {
+    display: inline-grid;
+    width: 600px;
+    height: 600px;
+    grid-template-columns: 38px auto auto auto auto auto auto auto auto 38px;
+    grid-template-rows: 38px auto auto auto auto auto auto auto auto 38px;
+}
+
+.board{
+    margin: auto;
+}
+
+.piece{
+    width: 50px;
+    height: auto;
+    grid-column: 2;
+}
+</style>
